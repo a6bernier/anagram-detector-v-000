1751db6e828603959518cc78d0a866ccfd4bcd8a
Andrew Bernier <a6bernier@gmail.com>
2018-10-11 20:17:31 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -16,5 +16,9 @@ end #ends class
 
 #take a word on initialization
 #instance should respond to a match method that takes an array of possible anagrams
+  #Iterate over the array of words
+  #try to determine if they are composed of the same letters (split into array?)
+  #compare the two using ==
+  #.sort ? to help comparison
 #return all matches in an array
 #if no matches in the array, return empy array
